153ad2ae31f76ecb6947622868d94f5d7a8641e7
Ralph Bean <rbean@redhat.com>
2012-08-21 12:25:28 -0400

    Lots of fixes.
    - Run as a thread to wait for other plugins.
    - Correctly find the meetbot plugin.
    - fedmsg initialization.
    - Bugfix to method wrapping.

--- supybot-fedmsg/plugin.py
+++ supybot-fedmsg/plugin.py
@@ -3,8 +3,13 @@
 :Author: Ralph Bean <rbean@redhat.com>
 """
 
-import types
+import datetime
+import fedmsg
+import socket
 import supybot.callbacks
+import threading
+import time
+import types
 
 
 class Fedmsg(supybot.callbacks.Plugin):
@@ -18,18 +23,46 @@ class Fedmsg(supybot.callbacks.Plugin):
     """
 
     def __init__(self, irc):
-        super(FedmsgPlugin, self).__init__(irc)
-        # TODO -- Check config file for the green light.
+        super(Fedmsg, self).__init__(irc)
+
+        # Initialize fedmsg resources.
+        fedmsg.init(name="supybot." + socket.gethostname())
+
+        # Launch in a thread to duckpunch *after* the other plugins
+        # have been setup.
+        thread = Injector()
+        thread.start()
+
+
+class Injector(threading.Thread):
+    """ Injector our code into other supybot plugins.
+
+    Wait 2 seconds before doing so to help make sure they've loaded
+    before we try.
+    """
+
+    def run(self):
+        # Sleep 2 seconds
+        time.sleep(2)
+
+        # Then, do our thing.
         self._duckpunch_meetbot()
+
         # TODO -- _duckpunch_announce()
 
-    def _duckpunch_meetbot(self):
+    def _duckpunch_meetbot(shmelf):
         """ Replace some of meetbot's methods with our own which simply call
         meetbot's original method, and then emit a fedmsg message before
         returning.
         """
 
-        from supybot.plugins.MeetBot.plugin import Class as target_class
+        try:
+            import sys
+            target_cls = sys.modules['MeetBot.meeting'].Meeting
+        except KeyError:
+            raise ValueError(
+                "MeetBot not yet enabled.  Try Fedmsg again later."
+            )
 
         tap_points = {
             'do_startmeeting': 'startmeeting',
@@ -38,23 +71,35 @@ class Fedmsg(supybot.callbacks.Plugin):
         for target_method, topic in tap_points.items():
 
             def wrapper_factory():
-                old_method = getattr(target_class, target_method).__func__
+                old_method = getattr(target_cls, target_method).__func__
 
                 def wrapper(self, *args, **kw):
+                    # Call the target plugin's original code first and save the
+                    # result.
                     result = old_method(self, *args, **kw)
+
+                    # Convert time.struct_time to a datetime object.
+                    # fedmsg.encoding can't yet handle struct_time.
+                    kw['time_'] = datetime.datetime(*kw['time_'][:-3])
+
                     # Emit on "org.fedoraproject.prod.meetbot.startmeeting"
-                    fedmsg.publish(topic=topic, msg=dict(args=args, kw=kw))
+                    fedmsg.publish(
+                        modname="meetbot",
+                        topic=topic,
+                        msg=dict(args=args, kw=kw),
+                    )
+                    # Return the original result from the target plugin.
                     return result
 
                 return wrapper
 
             # Build the new method and attach it to the target class.
-            new_method = types.MethodType(wrapper_factory(), target_class)
-            setattr(target_class, target_method, new_method)
+            new_method = wrapper_factory()
+            setattr(target_cls, target_method, new_method)
 
-    def _duckpunch_announce(self):
-        """ Instrument the announce code to emit messages. """
+    def _duckpunch_announce(shmelf):
+        """ Instrument the announce plugin to emit messages. """
         raise NotImplementedError
 
 
-Class = FedmsgPlugin
+Class = Fedmsg
