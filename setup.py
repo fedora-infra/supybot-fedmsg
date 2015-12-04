0ce59204f509d8a398cc64865f9273b2459662a3
Ralph Bean <rbean@redhat.com>
2015-12-04 09:49:15 -0500

    0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ f.close()
 
 setup(
     name='supybot-fedmsg',
-    version='0.1.0',
+    version='0.2.0',
     description="Supybot plugin for emitting events to the Fedora message bus",
     long_description=long_description,
     author='Ralph Bean',
